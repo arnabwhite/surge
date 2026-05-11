05c02eee4ea0ab10d9c39e6880c8ea5293efc328
4rnab <arnab.rayhanramdhani@gmail.com>
2026-05-11 16:23:41 +0700

    feat: tambah fungsi untuk memendekan url

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
+import boto3
+import uuid
+import os
 
 application = Flask(__name__)
 
@@ -8,7 +11,34 @@ def health_check():
 
 @application.route('/', methods=['GET'])
 def index():
-    return jsonify({"message": "URL Shortener API is running"}), 200
+    return render_template('index.html')
 
+@application.route('/shorten', methods=['POST'])
+def shorten_url():
+    data = request.get_json()
+    long_url = data.get('long_url')
+
+    if not long_url:
+        return jsonify({"error": "URL original diperlukan"}), 400
+
+    short_id = str(uuid.uuid4())[:6]
+
+    try:
+        table.put_item(
+            Item={
+                'short_id': short_id,
+                'long_url': long_url
+            }
+        )
+        
+        short_url = f"http://surge-app-env.eba-mvgjnfm4.ap-southeast-1.elasticbeanstalk.com/{short_id}"
+        return jsonify({
+            "short_id": short_id,
+            "short_url": short_url,
+            "original_url": long_url
+        }), 201
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
     application.run(debug=True, host='0.0.0.0', port=5000)  
