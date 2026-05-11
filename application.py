74f79c6c6e38077cf7a58d3fcf7dac3fb7b2331d
4rnab <arnab.rayhanramdhani@gmail.com>
2026-05-11 16:50:27 +0700

    fix: benerin dynamoDB

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 import boto3
 import uuid
 import os
 
 application = Flask(__name__)
 
+dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-1')
+table = dynamodb.Table('short-urls')
+
 @application.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({"status": "online"}), 200
+    return jsonify({"status": "online"} ), 200
 
 @application.route('/', methods=['GET'])
 def index():
@@ -20,6 +23,9 @@ def shorten_url():
 
     if not long_url:
         return jsonify({"error": "URL Needed"}), 400
+    
+    if not long_url.startswith(('http://', 'https://')):
+        long_url = 'https://' + long_url
 
     short_id = str(uuid.uuid4())[:6]
 
@@ -31,14 +37,33 @@ def shorten_url():
             }
         )
         
-        short_url = f"http://surge-app-env.eba-mvgjnfm4.ap-southeast-1.elasticbeanstalk.com/{short_id}"
+        base_url = request.host_url
+        short_url = f"{base_url}{short_id}"
+
         return jsonify({
             "short_id": short_id,
             "short_url": short_url,
             "original_url": long_url
         }), 201
-        #
+        
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@application.route('/<short_id>', methods=['GET'])
+def redirect_to_url(short_id):
+    try:
+        # Mencari data di DynamoDB berdasarkan short_id
+        response = table.get_item(Key={'short_id': short_id})
+        item = response.get('Item')
+
+        if item:
+            # Jika ketemu, arahkan browser ke URL asli yang disimpan
+            return redirect(item['long_url'])
+        else:
+            return "URL tidak ditemukan atau sudah kadaluwarsa.", 404
+            
+    except Exception as e:
+        return jsonify({"error": "Terjadi kesalahan pada server"}), 500
+    
 if __name__ == '__main__':
     application.run(debug=True, host='0.0.0.0', port=5000)  
